d047542f6da84fc36f929d1458a91deb0b0e7bfc
Igor Baldoino <igortthq@gmail.com>
2021-01-02 17:01:24 -0300

    Models and Administration

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField('Name', max_length=100)
+    price = models.DecimalField('Price', decimal_places= 2, max_digits=8)
+    stock = models.IntegerField('Available Stock Quantity')
+
+    def __str__(self):
+        return {self.name}
+
+class Client(models.Model):
+    name = models.CharField('Name', max_length=100)
+    surname = models.CharField('Surname', max_length=100)
+    email = models.EmailField('E-mail', max_length=100)
+
+    def __str__(self):
+        return f'{self.name} {self.surname}'
